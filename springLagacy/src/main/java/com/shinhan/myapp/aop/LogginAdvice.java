36f25d4b7a6b748810890049e5fa4fc1183c98cb
inetko2 <gkswhdgus98@gmail.com>
2024-12-05 14:55:03 +0900

    test

--- springLagacy/src/main/java/com/shinhan/myapp/aop/LogginAdvice.java
+++
@@ -1,5 +0,0 @@
-package com.shinhan.myapp.aop;
-
-public class LogginAdvice {
-
-}
--- springLagacy/src/main/java/net/firstzone/board2/TestController.java
+++
@@ -1,8 +0,0 @@
-package net.firstzone.board2;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class TestController {
-
-}
